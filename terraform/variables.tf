4d584441720da6d68cb54799adf4885c49c1b2ac
illusion4 <vladyslavlintur@gmail.com>
2025-11-21 16:55:44 +0200

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,7 +16,7 @@ variable "environment" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "eastus"
+  default     = "germanywestcentral"
 }
 
 variable "tags" {
